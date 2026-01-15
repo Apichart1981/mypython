df2cd86aa67c87cb88c946b4d58020bfe70e2a35
Apichart1981 <apichart.mongkolchart@gmail.com>
2026-01-15 17:04:12 +0700

    commit

---
+++ Day-1/ex001.py
@@ -0,0 +1,11 @@
+print("Hello Factory System")
+
+name = input("Enter operator name: ")
+shift = input("Enter shift (A/B/C): ")
+qty = input("Enter production qty: ")
+
+print("----- SUMMARY -----")
+print(f"Operator: {name}")
+print(f"Shift: {shift}")
+print(f"Qty: {qty} pcs")
+print("Status: Recorded")
